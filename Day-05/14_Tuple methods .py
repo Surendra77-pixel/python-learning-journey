d6753ff74a8be024db1b1f94920d42851e0fcb70
Surendra Manthri <surendramntr@gmail.com>
2026-04-08 01:05:13 +0530

    Update Day-05: Tuple methods

--- Day-05/14_Tuple methods .py
+++ Day-05/14_Tuple methods .py
@@ -17,3 +17,19 @@ print(my_tuple.count(2)) #output: 3 - The count() method is called on the tuple 
 my_tuple = (1, 2, 3, 4, 5)
 print(my_tuple.index(3)) #output: 2 - The index() method is called on the tuple my_tuple with the argument 3. It searches for the value 3 in the tuple and returns the index of its first occurrence, which is 2 (since indexing starts at 0). If we were to search for a value that is not in the tuple, such as 6, it would raise a ValueError.
 
+
+#3.tuple() constructor -
+
+# The tuple() constructor is used to create a new tuple from an iterable (like a list, string, or another tuple). It takes an iterable as an argument and returns a new tuple containing the elements of that iterable. Here is an example:
+my_list = [1, 2, 3, 4, 5]
+my_tuple = tuple(my_list)
+print(my_tuple) #output: (1, 2, 3, 4, 5) - The tuple() constructor is called with the list my_list as an argument. It converts the list into a tuple, resulting in the new tuple (1, 2, 3, 4, 5). The elements of the original list are now contained within the new tuple.
+
+
+#4.len() function -
+
+# The len() function is used to get the number of items in a tuple. It takes a tuple as an argument and returns the length of that tuple. Here is an example:
+my_tuple = (1, 2, 3, 4, 5)
+print(len(my_tuple)) #output: 5 - The len() function is called with the tuple my_tuple as an argument. It calculates the number of items in the tuple, which is 5, and returns that value. The output indicates that there are 5 elements in the tuple.
+
+
