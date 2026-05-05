e3569ce61048c4a82ad3ccb0a0b47e5f9c245c90
Surendra77-pixel <surendramntr@gmail.com>
2026-05-05 10:01:18 +0530

    Strings

---
+++ Day-10/09_String module.py
@@ -0,0 +1,24 @@
+
+#                      string module in python
+
+#The string module in Python provides a collection of functions and constants for working with strings. It is a built-in module that is always available in Python.
+
+#Some common functions and constants provided by the string module include:
+
+#1.ascii_letters:
+
+#The string.ascii_letters constant contains all the ASCII letters (both uppercase and lowercase). For example, string.ascii_letters will return 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.
+import string
+print(string.ascii_letters) #Output- 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+
+#2.digits:
+#The string.digits constant contains all the ASCII digits (0-9). For example, string.digits will return '0123456789'.
+import string
+print(string.digits) #Output- '0123456789'
+
+#3.punctuation:
+#The string.punctuation constant contains all the ASCII punctuation characters. For example, string.punctuation will return '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'.
+import string
+print(string.punctuation) #Output- '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
+
